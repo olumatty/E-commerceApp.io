4b5135f9156bab834f26bc771891eb351a61ab6b
Matthew Adebayo <olumatty@gmail.com>
2024-08-26 23:48:48 +0100

    last commit

--- src/Components/CartItems/cartItems.jsx
+++ src/Components/CartItems/cartItems.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from "../../Context/ShopContext";
 import remove_icon from "../Assets/cart_cross_icon.png";
 
 const CartItems = () => {
-  const {all_product, cartItems, removeFromCart } = useContext(ShopContext);
+  const {getTotalCartAmount, all_product, cartItems, removeFromCart } = useContext(ShopContext);
   return (
     <div className="cartItems">
       <div className="cartitem-format-main">
@@ -15,25 +15,65 @@ const CartItems = () => {
         <p> Total </p>
         <p> Remove </p>
       </div>
-      <hr/>
+      <hr />
       {all_product.map((e) => {
         if (cartItems[e.id] > 0) {
-            return <div key ={e.id}> 
-                <div className="cartitems-format cartitem-format-main">
-                  <img src={e.image} alt="" className="carticon-product-icon" />
-                  <p>{e.name}</p>
-                  <p>${e.new_price}</p>
-                  <button className="cartitems-quantity">{cartItems[e.id]} </button>
-                  <p>${e.new_price*cartItems[e.id]}</p>
-                  <img src={remove_icon} onClick={() => {removeFromCart(e.id)}} alt="" className="cartItems-remove-icon"/>
-                </div>
-                <hr/>
+          return (
+            <div key={e.id}>
+              <div className="cartitems-format cartitem-format-main">
+                <img src={e.image} alt="" className="carticon-product-icon" />
+                <p>{e.name}</p>
+                <p>${e.new_price}</p>
+                <button className="cartitems-quantity">
+                  {cartItems[e.id]}
+                </button>
+                <p>${e.new_price * cartItems[e.id]}</p>
+                <img
+                  src={remove_icon}
+                  onClick={() => {
+                    removeFromCart(e.id);
+                  }}
+                  alt=""
+                  className="cartItems-remove-icon"
+                />
+              </div>
+              <hr />
             </div>
-        } 
-
+          );
+        }
+        return null;
       })}
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Total</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a promo code, Enter it here</p>
+          <div className="cartitems-promobox">
+            <input type="text" placeholder="promo code" />
+            <button>submit</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default CartItems;
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -3,33 +3,62 @@ import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
-  const contextValue = { all_product };
-  const getDefaultCart = () => {
-    let cart = {};
-    for (let index = 0; index < all_product.length + 1; index++) {
-      cart[index] = 0;
-    }
-    return cart;
+const getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < all_product.length + 1; index++) {
+    cart[index] = 0;
   }
-  const ShopContextProvider = (props) => {
-    const [cartItems, setcartItems] = useState(getDefaultCart());
+  return cart;
+};
+const ShopContextProvider = (props) => {
+  const [cartItems, setcartItems] = useState(getDefaultCart());
 
-    const addToCart = (itemId) => {
-      setcartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-      console.log(cartItems);
+  const addToCart = (itemId) => {
+    setcartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setcartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
     }
+    return totalAmount;
+  }
 
-    const removeFromCart = (itemId) => {
-      setcartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
-    };
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+        if(cartItems[item] > 0)
+          {
+          totalItem+= cartItems[item];
+        }
+      }
+    return totalItem;
+    }
+   
+  const contextValue = {
+    getTotalCartAmount,
+    getTotalCartItems,
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
 
-    const contextValue = {all_product, cartItems, addToCart, removeFromCart};
-  
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
     </ShopContext.Provider>
-  )
-}
+  );
+};
 
 export default ShopContextProvider;
